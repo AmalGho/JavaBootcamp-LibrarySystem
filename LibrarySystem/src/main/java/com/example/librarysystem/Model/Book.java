a847ef700efc179032c41d7eb05cf8b944becc90
Amal <amol43@outlook.com>
2023-08-28 15:51:05 +0300

    add validations

--- LibrarySystem/src/main/java/com/example/librarysystem/Model/Book.java
+++ LibrarySystem/src/main/java/com/example/librarysystem/Model/Book.java
@@ -2,11 +2,14 @@ package com.example.librarysystem.Model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Setter
@@ -18,14 +21,25 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotEmpty(message = "book title should not be empty")
+    @Length(max = 30, message = "book title length should not be more than 30 characters")
+    @Column(columnDefinition = "varchar(30) not null")
     private String title;
+
+    @NotEmpty(message = "book author should not be empty")
+    @Length(max = 20, message = "book author length should not be more than 20 characters")
+    @Column(columnDefinition = "varchar(20) not null")
     private String author;
 
     @Column(columnDefinition = "varchar(10) check (category = \"Academic\" or category = \"Mystery\" or category = \"Novel\")")
     @Pattern(regexp = "\\W*((?i)Academic|Mystery|Novel(?-i))\\W*", message = "category not available in the library")
     private String category;
+
+    @NotNull(message = "ISBN should not be empty")
+    @Column(columnDefinition = "int not null")
     private Integer isbn;
 
     @Min(value = 50, message = "number of page must be 50 pages and more")
+    @Column(columnDefinition = "int check (numberOfPages >= 50)")
     private Integer numberOfPages;
 }
--- LibrarySystem/src/main/java/com/example/librarysystem/Model/Librarian.java
+++ LibrarySystem/src/main/java/com/example/librarysystem/Model/Librarian.java
@@ -2,11 +2,13 @@ package com.example.librarysystem.Model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Setter
@@ -18,7 +20,14 @@ public class Librarian {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotEmpty(message = "name should not be empty")
+    @Length(min = 4, message = "name length should be more than 3 characters")
+    @Column(columnDefinition = "varchar(10) not null check (name > 3)")
     private String name;
+
+    @NotEmpty(message = "username should not be empty")
+    @Length(min = 5, message = "name length should be more than 4 characters")
+    @Column(columnDefinition = "varchar(10) not null unique check (name > 4)")
     private String username;
 
     @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "enter strong password!!")
